8a0ad7ccf4600ac6e94ab711f30e6989f273cbd3
decastrojeric <jericgithub@gmail.com>
2026-07-08 16:31:03 +0800

    Week 3 Day 1 - Analytics Service Unit Testing (#22)
    * Week 3 Day 1 - Analytics Service Unit Testing
    
    * Refactor Analytics Service to use Booking Model
    
    * Integrate Analytics Service into Booking Service
    
    * Integrate Analytics Service into Booking Service
    
    * Refactor analytics service to compute metrics from booking model
    
    ---------
    
    Co-authored-by: MFranklinC <23-15964@g.batstate-u.edu.ph>

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -1,11 +1,41 @@
-// Mock Database
-export const bookings = [];
+export const bookings = [
+  {
+    id: 1,
+    clientName: "Franklin",
+    eventDate: "2026-01-15",
+    month: "Jan",
+    amount: 30000,
+    rating: 5,
+    status: "Completed",
+  },
+  {
+    id: 2,
+    clientName: "Jeric",
+    eventDate: "2026-02-10",
+    month: "Feb",
+    amount: 15000,
+    rating: 4,
+    status: "Completed",
+  },
+  {
+    id: 3,
+    clientName: "Mark",
+    eventDate: "2026-03-08",
+    month: "Mar",
+    amount: 3500,
+    rating: 5,
+    status: "Pending",
+  },
+];
 
-// Create Booking
 export function createBooking(data) {
   const booking = {
     id: bookings.length + 1,
-    ...data,
+    clientName: data.clientName,
+    eventDate: data.eventDate,
+    month: data.month,
+    amount: data.amount,
+    rating: data.rating,
     status: "Pending",
   };
 
@@ -14,12 +44,10 @@ export function createBooking(data) {
   return booking;
 }
 
-// Get Booking by ID
 export function getBookingById(id) {
   return bookings.find((booking) => booking.id === Number(id));
 }
 
-// Update Booking Status
 export function updateBookingStatus(id, status) {
   const booking = getBookingById(id);
 
@@ -30,4 +58,8 @@ export function updateBookingStatus(id, status) {
   booking.status = status;
 
   return booking;
+}
+
+export function getAllBookings() {
+  return bookings;
 }
