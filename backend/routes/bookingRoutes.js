4ed08faa034d731ffcb725ecec23f1d11b13c0a9
MFranklinC <23-15964@g.batstate-u.edu.ph>
2026-07-15 09:14:15 +0800

    Week 4 Day 2 - Gallery API

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -7,13 +7,10 @@ import {
 
 const router = express.Router();
 
-// Create a new booking
 router.post("/bookings", createBooking);
 
-// Get booking by ID
 router.get("/bookings/:id", getBookingById);
 
-// Update booking status
 router.put("/bookings/:id/status", updateBookingStatus);
 
 export default router;
