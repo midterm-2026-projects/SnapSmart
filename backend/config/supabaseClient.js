fb0833e58384116fd70e2e67dfc943b0a634666f
MFranklinC <23-15964@g.batstate-u.edu.ph>
2026-07-21 17:49:36 +0800

    Week 6: Day 1 - End to End (Fixed Backend)

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -2,13 +2,6 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { createClient } from "@supabase/supabase-js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
